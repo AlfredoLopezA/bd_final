cf71c7598b714de7ca2c9a90a7fcf74342df2a6c
DESKTOP-9P2MIT9\Alfredo <alfredo.lopez@usach.cl>
2017-11-30 19:25:40 -0300

    cambios config

--- config/application.rb
+++ config/application.rb
@@ -6,11 +6,10 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module BarMoe
+module RailsProject
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
-
+    config.api_only = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
